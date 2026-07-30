286d7b00653640774045c7f21c1fe05ab2a30ef2
rameezraja <rameezrajaoficial5@gmail.com>
2026-07-30 22:12:02 +0500

    Setup database configuration using .env

---
+++ mentorship_phase0_capstone_project_rameez/Student_Record_Management_System/database.py
@@ -0,0 +1,24 @@
+#we are importing os to use the .env file to protect database password
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()           #it loads the env file into environment
+
+
+#python and sqlalchemy part start here:
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+
+DATABASE_URL="postgresql://postgres:rameezpostgres78@localhost:5432/Student_Record_Management_System"
+
+engine=create_engine(DATABASE_URL)
+
+SessionLocal=sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
