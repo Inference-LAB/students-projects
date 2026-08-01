251bd06202f6f8b5b04f6dec1da19783e8fba38b
rameezraja <rameezrajaoficial5@gmail.com>
2026-08-01 20:04:03 +0500

    Add CSV export feature for all tables and modified gitignore

--- mentorship_phase0_capstone_project_rameez/student_record_Mmanagement_system/main.py
+++ mentorship_phase0_capstone_project_rameez/student_record_Mmanagement_system/main.py
@@ -6,34 +6,45 @@ from crud import (
     view_students,
     update_student,
     delete_student,
+
     create_course,
     view_courses,
     update_course,
     delete_course,
+
     enroll_student,
     view_enrollments,
     update_enrollment,
     delete_enrollment
 
 )
+                          #import all 3 functions for exporting csv
+from exports.exports_csv import (
+    export_students_to_csv,
+    export_courses_to_csv,
+    export_enrollments_to_csv
+)
 
 
 def menu():
-                       #student functions
+                       #student 
     print("1. Create Student")
     print("2. View Students")
     print("3. Update Student")
     print("4. Delete Student")
-                       #course functions
+                       #course 
     print("5. Create Course")
     print("6. View Courses")
     print("7. Update Course")
     print("8. Delete Course")
-                       #enrollment functions
+                       #enrollment 
     print("9. Enroll Student")
     print("10. View Enrollments")
     print("11. Update Enrollment")
     print("12. Delete Enrollment")
+
+                       #export table in csv
+    print("13. Export Table To CSV")
 
     print("0. Exit")
 
@@ -82,11 +93,31 @@ while True:
     elif choice == "12":
         delete_enrollment()
 
+    elif choice == "13":
+        print("1. Export Students")
+        print("2. Export Courses")
+        print("3. Export Enrollments")
+
+        export_choice = input("enter your choice:")
+
+        if export_choice == "1":
+            export_students_to_csv()
+
+        elif export_choice == "2":
+            export_courses_to_csv()
+
+        elif export_choice == "3":
+            export_enrollments_to_csv()
+        else:
+            print("Invalid export choice.")
+
+
+
     elif choice == "0":
         print("Thank you for using Student Record Management System.")
 
-                                   #while loop ends here
-        break
+        break                      #while loop ends here
+        
 
     else:
         print("Invalid choice. Please try again.")
